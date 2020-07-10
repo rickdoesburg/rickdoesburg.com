9054ddd2556394c103d852bfe11e3da40744989a
Rick Doesburg <rickdoesburg@gmail.com>
2020-07-10 14:52:23 +0200

    Fix mobile overflow

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,15 @@
-$('.input-toggle input[type=checkbox]').on('change', function() {     
-     $('body').toggleClass('darkmode-pls');        
+document.addEventListener('DOMContentLoaded', function(event) {
+    const darkmodeEnabler = document.querySelector('.darkmode-enabler');
+
+    if(darkmodeEnabler) {
+        const darkmodeToggle = darkmodeEnabler.querySelector('input[type=checkbox]');
+
+        darkmodeToggle.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                document.querySelector('body').classList.add('darkmode-pls');
+            } else {
+                document.querySelector('body').classList.remove('darkmode-pls');
+            }
+        })
+    }
 });
