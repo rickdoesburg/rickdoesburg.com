43cdb662e41958335e82c0b671b77f7146d5f176
Rick Doesburg <rickdoesburg@gmail.com>
2023-08-13 17:35:10 +0200

    Updates, fixed dark mode toggle

--- dist/all.js
+++ dist/all.js
@@ -1,8 +1,11 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
     const darkmodeEnabler = document.querySelector('.darkmode-enabler');
 
     if(darkmodeEnabler) {
         const darkmodeToggle = darkmodeEnabler.querySelector('input[type=checkbox]');
+        const prefersDarkMode = window.matchMedia("(prefers-color-scheme:dark)").matches; // true
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) { console.log('changed!!');})
 
         darkmodeToggle.addEventListener('change', (e) => {
             if (e.target.checked) {
@@ -11,6 +14,11 @@ document.addEventListener('DOMContentLoaded', function(event) {
                 document.querySelector('body').classList.remove('darkmode-pls');
             }
         });
+
+        if(prefersDarkMode) {
+            darkmodeToggle.checked = true;
+            document.querySelector('body').classList.add('darkmode-pls');
+        }
     }
 });
 
