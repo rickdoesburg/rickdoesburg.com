d221899009bb6f085d24c5c821869849cb3b1183
Rick Doesburg 🍕 <rdoesburg@ilionx.com>
2026-03-05 10:03:33 +0100

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,100 @@
-const gulp      = require('gulp');
-
-const jshint    = require('gulp-jshint');
+const gulp = require("gulp");
+const esbuild = require("gulp-esbuild");
 const sassProcessor = require("gulp-dart-sass");
-const concat    = require('gulp-concat');
+const concat = require("gulp-concat");
+const fileInclude = require("gulp-file-include");
+const bs = require("browser-sync").create();
 
-const terser = require('gulp-terser');
-const rename    = require('gulp-rename');
-const fileInclude = require('gulp-file-include');
-const bs        = require('browser-sync').create()
-
-gulp.task('lint', function() {
-    return gulp.src('assets/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task("compile-js", function () {
+    return gulp.src("assets/js/*.js").pipe(concat("all.js")).pipe(gulp.dest("dist"));
 });
 
-gulp.task('sass', function() {
-    return gulp.src('build-scss/*.scss')
-        .pipe(sassProcessor().on("error", sassProcessor.logError))
-        .pipe(gulp.dest('dist/css'))
-        // Reload BS
-        .pipe(bs.reload({stream: true}));
+gulp.task("sass", function () {
+    return (
+        gulp
+            .src("build-scss/*.scss")
+            .pipe(sassProcessor().on("error", sassProcessor.logError))
+            .pipe(gulp.dest("dist/css"))
+            // Reload BS
+            .pipe(bs.reload({ stream: true }))
+    );
 });
 
 // Compile html and move to dist
-gulp.task('compile-html', function() {
-  return gulp.src(['components/pages/*.html'])
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(bs.reload({stream: true}));
+gulp.task("compile-html", function () {
+    return gulp
+        .src(["components/pages/*.html"])
+        .pipe(
+            fileInclude({
+                prefix: "@@",
+                basepath: "@file",
+            }),
+        )
+        .pipe(gulp.dest("./dist/"))
+        .pipe(bs.reload({ stream: true }));
 });
 
-gulp.task('scripts', function() {
-    return gulp.src('assets/js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(terser())
-        .pipe(gulp.dest('dist/js'));
+gulp.task("scripts", function () {
+    return gulp
+        .src("dist/all.js")
+        .pipe(
+            esbuild({
+                outfile: "all.min.js",
+                minify: true,
+                target: "es2015",
+            }),
+        )
+        .pipe(gulp.dest("dist/js"));
 });
 
 // Copy assets to dist
-gulp.task('copy', function() {
-    return gulp.src(['./assets/**/*'], { encoding: false })
-    .pipe(gulp.dest('./dist'));
+gulp.task("copy", function () {
+    return gulp.src(["./assets/**/*"], { encoding: false }).pipe(gulp.dest("./dist"));
 });
 
 // Copy favicons to root
-gulp.task('favicons', function() {
-    return gulp.src('./assets/img/favicons/*', {encoding: false})
-    .pipe(gulp.dest('./dist'));
+gulp.task("favicons", function () {
+    return gulp.src("./assets/img/favicons/*", { encoding: false }).pipe(gulp.dest("./dist"));
 });
 
 // Copy manifest to root
-gulp.task('manifest', function() {
-    return gulp.src('./manifest.json', './serviceWorker.js')
-    .pipe(gulp.dest('./dist'));
+gulp.task("manifest", function () {
+    return gulp.src("./manifest.json", "./serviceWorker.js").pipe(gulp.dest("./dist"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('assets/js/*.js', gulp.series('lint', 'scripts'));
-    gulp.watch('atomic-scss/**/*.scss', gulp.parallel('sass'));
-    gulp.watch('components/**/*.html', gulp.series('compile-html'));
+gulp.task("watch", function () {
+    gulp.watch("assets/js/*.js", gulp.series("compile-js", "scripts"));
+    gulp.watch("atomic-scss/**/*.scss", gulp.parallel("sass"));
+    gulp.watch("components/**/*.html", gulp.series("compile-html"));
 });
 
 // Syncin dat browser
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function () {
     bs.init({
         // I don't proxy shit (in this project)
         // proxy: 'localhost:9966', // makes a proxy for localhost:8080
         server: {
-            baseDir: "./dist/"
+            baseDir: "./dist/",
         },
 
         // Fancy QR for mobile testing
-        plugins: ['bs-console-qrcode']
+        plugins: ["bs-console-qrcode"],
     });
 });
 
 // gulp.task('default', gulp.parallel('lint', 'sass', 'scripts', 'watch'));
-gulp.task('build', gulp.parallel('lint', 'sass', 'scripts', 'compile-html', 'favicons', 'manifest', 'copy'));
-gulp.task('default', gulp.parallel('browser-sync','lint', 'sass', 'scripts', 'compile-html', 'favicons', 'manifest', 'copy', 'watch'));
+gulp.task("build", gulp.parallel("compile-js", "sass", "scripts", "compile-html", "favicons", "manifest", "copy"));
+gulp.task(
+    "default",
+    gulp.parallel(
+        "browser-sync",
+        "compile-js",
+        "sass",
+        "scripts",
+        "compile-html",
+        "favicons",
+        "manifest",
+        "copy",
+        "watch",
+    ),
+);
